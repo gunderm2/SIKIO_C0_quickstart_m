ef671e3ce513dc2a12325c0998a9991f7bc5ff64
gunderm2 <tomichaelpg@gmail.com>
2015-03-15 21:48:36 -0500

    SIKIO_C0_quickstart_m
    Original submit

---
+++ src/ioio/examples/hello/MainActivity.java
@@ -0,0 +1,133 @@
+/*
+ * Copyright 2015 Michael Gunderson. All rights reserved.
+ *
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are
+ * permitted provided that the following conditions are met:
+ *
+ *    1. Redistributions of source code must retain the above copyright notice, this list of
+ *       conditions and the following disclaimer.
+ *
+ *    2. Redistributions in binary form must reproduce the above copyright notice, this list
+ *       of conditions and the following disclaimer in the documentation and/or other materials
+ *       provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED "AS IS" AND ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL ARSHAN POURSOHI OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * The views and conclusions contained in the software and documentation are those of the
+ * authors and should not be interpreted as representing official policies, either expressed
+ * or implied.
+ */
+package ioio.examples.hello;
+
+import ioio.lib.api.DigitalOutput;
+import ioio.lib.api.exception.ConnectionLostException;
+import ioio.lib.util.BaseIOIOLooper;
+import ioio.lib.util.IOIOLooper;
+import ioio.lib.util.android.IOIOActivity;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.ToggleButton;
+
+/**
+ * This is the main activity of the HelloIOIO example application.
+ * 
+ * It displays a toggle button on the screen, which enables control of the
+ * on-board LED. This example shows a very simple usage of the IOIO, by using
+ * the {@link IOIOActivity} class. For a more advanced use case, see the
+ * HelloIOIOPower example.
+ */
+public class MainActivity extends IOIOActivity {
+	private TextView textView;
+	boolean lightOn = false;
+	/**
+	 * Called when the activity is first created. Here we normally initialize
+	 * our GUI.
+	 */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+		textView = (TextView) findViewById(R.id.title);
+	}
+
+	/**
+	 * This is the thread on which all the IOIO activity happens. It will be run
+	 * every time the application is resumed and aborted when it is paused. The
+	 * method setup() will be called right after a connection with the IOIO has
+	 * been established (which might happen several times!). Then, loop() will
+	 * be called repetitively until the IOIO gets disconnected.
+	 */
+	class Looper extends BaseIOIOLooper {
+		/** The on-board LED. */
+		private DigitalOutput led;
+
+		/**
+		 * Called every time a connection with IOIO has been established.
+		 * Typically used to open pins.
+		 * 
+		 * @throws ConnectionLostException
+		 *             When IOIO connection is lost.
+		 * 
+		 * @see ioio.lib.util.AbstractIOIOActivity.IOIOThread#setup()
+		 */
+		@Override
+		protected void setup() throws ConnectionLostException {
+			led = ioio_.openDigitalOutput(0, true);
+		}
+
+		/**
+		 * Called repetitively while the IOIO is connected.
+		 * 
+		 * @throws ConnectionLostException
+		 *             When IOIO connection is lost.
+		 * 
+		 * @see ioio.lib.util.AbstractIOIOActivity.IOIOThread#loop()
+		 */
+		@Override
+		public void loop() throws ConnectionLostException {
+			try {
+				led.write(lightOn); // Turn the LED on or off
+				setTextColor(lightOn); //Change TextView text color with LED change
+			    lightOn = !lightOn; // Change the lightOn from true to false or vice versa
+			    Thread.sleep(500); // Don't call this loop again for 500 milliseconds
+			} catch (InterruptedException e) {
+			}
+		}
+	}
+
+	/**
+	 * A method to create our IOIO thread.
+	 * 
+	 * @see ioio.lib.util.AbstractIOIOActivity#createIOIOThread()
+	 */
+	@Override
+	protected IOIOLooper createIOIOLooper() {
+		return new Looper();
+	}
+	
+	private void setTextColor(final boolean condition) {
+		runOnUiThread(new Runnable() {
+			@Override
+			public void run() {
+				if(condition) {
+//					textView.setTextColor(Color.WHITE);
+					textView.setBackgroundColor(Color.BLACK);
+				} else {
+//					textView.setTextColor(Color.YELLOW);
+					textView.setBackgroundColor(Color.YELLOW);
+				}
+				
+			}
+		});
+	}
+}
